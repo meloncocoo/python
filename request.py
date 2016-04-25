9205cf264c57029077cdf8311a62dcd2df8ea9bb
melon <melon@localhost.localdomain>
2016-04-25 18:03:56 +0800

    +add reqeust.py for reqeust a url

---
+++ request.py
@@ -0,0 +1,16 @@
+import sys
+
+def main(url):
+	print('start main function with %(url)s' % {'url': url})
+
+if __name__ == '__main__':
+	baseurl = ''
+	if sys.argv[1:]:
+		baseurl = sys.argv[1]
+		if baseurl.startswith("http://"):
+			main(baseurl)
+		else:
+			print("Parameter Error!")
+	else:
+		print("Parameter request.")
+	
